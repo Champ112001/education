8063f6293b093a5ed686fb6f843893ca302aec26
Champ112001 <buba130598@gmail,com>
2021-02-13 23:20:09 +0400

    js

---
+++ main.js
@@ -0,0 +1,8 @@
+// document.write("JS говорит привет!")
+// console.log("Привет, Анёчек!!")
+// console.info("Привет, любимая девочка!")
+// console.error("js")
+// console.warn("js")
+
+var number = 5;
+console.log("Переменная: " + number)
